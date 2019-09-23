f1664fd91442aaf6e2cbee7d71dcf59f9948383c
abdul-aziz1 <abdul.aziz353031@gmail.com>
2019-09-23 06:37:38 +0600

    Form modified

--- Form/Form.php
+++ Form/Form.php
@@ -1,9 +1,9 @@
 <?php
-
-
+    echo '<pre>';
+    print_r($_POST);
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
 <title>
@@ -25,6 +25,39 @@
                 <td><button type="submit" value="Submit">Submit</button></td>
             </tr>
 
+        </table>
+    </form>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+    get-post
+</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <table>
+            <tr>
+                <td>First Number</td>
+                <td><input type="text" name="first_valu"></td>
+            </tr>
+            <tr>
+                <td>Last Name</td>
+                <td><input type="text" name="second_valu"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" name="btn" value="+">
+                    <input type="submit" name="btn" value="-">
+                    <input type="submit" name="btn" value="*">
+                    <input type="submit" name="btn" value="/">
+                    <input type="submit" name="btn" value="%">
+                </td>
+            </tr>
         </table>
     </form>
 </body>
